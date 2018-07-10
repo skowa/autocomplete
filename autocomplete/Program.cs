3301de13126910730058704582bfaa5f319d11e3
Alexandra Gushchenskova <skowa98@gmail.com>
2018-07-10 15:27:17 +0300

    add program.cs

---
+++ autocomplete/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace autocomplete
+{
+	public class Program
+	{
+		private static AutocompleteFromFileHelper helper = new AutocompleteFromFileHelper("input.txt");
+		private static Autocomplete autocomplete = new Autocomplete();
+
+		static void Main(string[] args)
+		{
+			List<string> hints = helper.GetHints();
+
+			string input = string.Empty;
+			ConsoleKeyInfo keyInfo;
+
+			do
+			{
+				keyInfo = Console.ReadKey(true);
+				input += keyInfo.KeyChar.ToString();
+
+				if (input.Length != 0)
+				{
+					List<string> foundElements = autocomplete.AutocompleteString(input, hints);
+
+					Console.Clear();
+					Console.WriteLine(input);
+					foreach (string foundElement in foundElements)
+					{
+						Console.WriteLine(foundElement);
+					}
+				}
+			} while (keyInfo.Key != ConsoleKey.Enter);
+		}
+	}
+}
